6ba70b88496c4037871f263e02296e7a1a7b2c82
Marko Michinaux <marko.michinaux@vascarsolutions.com>
2022-05-19 19:05:31 -0400

    feat: wip add graphql for users and auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  login() {
+    return null;
+  }
+  logout() {
+    return null;
+  }
+  signUp() {
+    return null;
+  }
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,9 @@
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+
+@ObjectType({ description: 'user' })
+export class User {
+  @Field(() => ID)
+  id: string;
+  @Field()
+  username: string;
+}
